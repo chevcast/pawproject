c73ae270e534892d127aa75490fee5948da8b5f4
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-06-05 14:26:48 -0600

    Added Express 4 and mongoose-simpledb.

---
+++ dbmodels/Role.js
@@ -0,0 +1,11 @@
+exports.schema = {
+    name: String,
+    description: String
+};
+
+exports.methods = {
+    // Get all users with the specific role.
+    users: function () {
+        //todo 
+    }
+};
