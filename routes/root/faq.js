f9e50e524a6ec2ee4b2f196e89c9c7339115d9f4
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-06-15 14:01:57 -0600

    Added FAQ routes.

---
+++ routes/root/faq.js
@@ -0,0 +1,3 @@
+exports.index = function (req, res) {
+    res.render('root/faq', { title: 'Paw Project FAQ' });
+};
--- routes/root/index.js
+++ routes/root/index.js
@@ -6,4 +6,13 @@ router.get('/', require('./home'));
 // Main Blog
 router.get('/blog', require('./blog'));
 
+// FAQ
+var faqHandlers = require('./faq');
+router.get('/faq', faqHandlers.index);
+// TODO: Figure out how ngResource works in detail and craft the following routes to adhere to their paradigm.
+router.get('/faq/getAll', faqHandlers.getAll);
+router.post('/faq/addNew', faqHandlers.addNew);
+router.post('/faq/update', faqHandlers.update);
+router.post('/faq/delete', faqHandlers.delete);
+
 module.exports = router;
