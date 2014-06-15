1c6244e4044e42d20214d9ded243eda628ff61b9
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-06-15 14:06:41 -0600

    Updated FAQ view content and removed unfinished route handlers.

--- routes/root/index.js
+++ routes/root/index.js
@@ -10,9 +10,11 @@ router.get('/blog', require('./blog'));
 var faqHandlers = require('./faq');
 router.get('/faq', faqHandlers.index);
 // TODO: Figure out how ngResource works in detail and craft the following routes to adhere to their paradigm.
-router.get('/faq/getAll', faqHandlers.getAll);
-router.post('/faq/addNew', faqHandlers.addNew);
-router.post('/faq/update', faqHandlers.update);
-router.post('/faq/delete', faqHandlers.delete);
+/*
+ *router.get('/faq/getAll', faqHandlers.getAll);
+ *router.post('/faq/addNew', faqHandlers.addNew);
+ *router.post('/faq/update', faqHandlers.update);
+ *router.post('/faq/delete', faqHandlers.delete);
+ */
 
 module.exports = router;
