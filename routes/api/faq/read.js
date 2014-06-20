59150a06cd3d0a4e209f36eadc943199e0cd065d
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-06-19 23:21:25 -0600

    Modified sample questions to test page layout.

--- routes/api/faq/read.js
+++ routes/api/faq/read.js
@@ -1,12 +1,12 @@
 module.exports = function (req, res) {
     res.send([
         {
-            question: 'What is the meaning of life, the universe, and everythign?',
+            question: 'What is the meaning of life, the universe, and everything?',
             answer: '42'
         },
         {
             question: 'Test Question?',
-            answer: 'Yes.',
+            answer: 'A longer answer that takes up a lot more of its container, but hopefully continues to look elegant on the page as the text is okay to flow throuhgout. The user should not be limited on how much text they can place in this field.',
         }
     ]);
 };
