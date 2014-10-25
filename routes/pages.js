8156122c4376381a27cf6b0c9065a85e363d86bb
Alex Ford <Alex.Ford@CodeTunnel.com>
2014-10-25 14:42:56 -0600

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -11,5 +11,8 @@ router.get('/youcanhelp', pages.youCanHelp);
 router.get('/team', pages.team);
 router.get('/contact', pages.contact);
 router.get('/legislation', pages.legislation);
+router.get('/tshirtcontest', function (req, res) {
+  res.redirect('http://www1.pawproject.org/tshirtcontest');
+});
 
 module.exports = router;
